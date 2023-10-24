db2e0b4646e24bb4adf18d6bcd1a8497fb7b4c4c
hunter23230 <144774024+hunter23230@users.noreply.github.com>
2023-10-24 11:50:58 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://teachablemachine.withgoogle.com/models/bXy2kDNi/';
+let imageModelURL = 'https://hunter23230.github.io/computer1/';
 
 // Video
 let video;
@@ -26,45 +26,47 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(320, 260);
+
+  createCanvas(320, 320);
+
+  
+
   // Create the video
-  video = createCapture(VIDEO);
-  video.size(320, 240);
+
+    var constraints = {
+
+    audio: false,
+
+    video: {
+
+      facingMode: {
+
+        exact: "environment"
+
+      }
+
+    }   
+
+    //video: {
+
+      //facingMode: "user"
+
+    //}
+
+  };
+
+  video = createCapture(constraints);
+
+  video.size(320, 320);
+
   video.hide();
 
+
+
   flippedVideo = ml5.flipImage(video)
+
   // Start classifying
+
   classifyVideo();
-}
 
-function draw() {
-  background(0);
-  // Draw the video
-  image(flippedVideo, 0, 0);
-
-  // Draw the label
-  fill(255);
-  textSize(16);
-  textAlign(CENTER);
-  text(label, width / 2, height - 4);
-}
-
-// Get a prediction for the current video frame
-function classifyVideo() {
-  flippedVideo = ml5.flipImage(video)
-  classifier.classify(flippedVideo, gotResult);
-}
-
-// When we get a result
-function gotResult(error, results) {
-  // If there is an error
-  if (error) {
-    console.error(error);
-    return;
-  }
-  // The results are in an array ordered by confidence.
-  // console.log(results[0]);
-  label = results[0].label;
-  // Classifiy again!
-  classifyVideo();
 }
